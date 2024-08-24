8635c42a7d1d7f3e6f6bb19362691ca4aff81331
saiket-das <saiketdas02@gmail.com>
2024-08-24 14:37:44 +0600

    Operators

---
+++ src/2.Operators.kt
@@ -0,0 +1,29 @@
+fun main() {
+    var a: Int = 21;
+    var b: Int = 10;
+
+    // Arithmetic operators
+    println(a + b);    // Addition (31)
+    println(a - b);    // Subtraction (11)
+    println(a * b);    // Multiplication (210)
+    println(a.toFloat() / b);    // Division (2.1)
+    println(a % b);    // Division
+
+    // Relational operators
+    println(a >  b);    // Greater than (true)
+    println(a < b);     // Less than (false)
+    println(a >= b);    // Greater than or Equal to (true)
+    println(a <= b);    // Less than or Equal to (false)
+    println(a == b);    // Equal to (false)
+    println(a != b);    // Equal to (true)
+
+    // Logical operators
+    // &&    Logical and
+    // ||    Logical or
+    // !     Logical not
+
+    a++;    // Increment
+    b--;    // Decrement
+    ++a;    // Pre increment
+    --b;    // Pre decrement
+}
