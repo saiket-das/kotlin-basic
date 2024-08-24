92ce93d5e19206c04c05f0d33e4db0c81150dc73
saiket-das <saiketdas02@gmail.com>
2024-08-24 15:05:06 +0600

    If else

---
+++ src/3.if_else.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val x: Int = 70;
+    val y: Int = 80;
+
+    if (x > y) {
+        println("X is greater than Y");
+    }
+    else if (x < y) {
+        println("X is less than Y");
+    }
+    else {
+        println("x equal to y");
+    }
+}
