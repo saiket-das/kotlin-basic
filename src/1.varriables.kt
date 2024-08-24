a9917259e8b1e2e785b70a58f37a9acc8eb90ad9
saiket-das <saiketdas02@gmail.com>
2024-08-24 14:13:32 +0600

    First commit

---
+++ src/1.varriables.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val place: String = "New York";
+    var age: Int = 22;
+    var pi: Double = 3.1416
+    var isTrue: Boolean = false;
+    var alphabet: Char = 'A';
+    var message: String = "Hello World!";
+
+    // place = "Dhaka"    // Val cannot be reassigned
+    age = 20;
+
+    println(place);
+    println(age);
+}
---
+++ src/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    println("Hello World!")
+    print(1+2);
+    print(false);
+}
